4f9e9c83f0e4dd1f46c1b2724d75004604143d2c
Gayathri S <gayathrisubramanian2006@gmail.com>
2026-09-22 22:12:06 +0530

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from tkinter import *
-win = Tk()
+from tkinter import *                             # for GUI
+win = Tk()                                             # window is created
+win.title("Weather App")    
+win.config(bg = "lightblue")
+win.geometry("500x500")
 
-win.mainloop()
+name_label = Label(win, text="Weather App",font=('Arial', 20, 'bold'))  
+
+name_label.place(x=50,y=50, height=50, width=400)
+win.mainloop()                               # window continues until user interacts
